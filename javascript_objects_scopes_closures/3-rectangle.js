5fc7d6c6512a5b9a523e9202d1d4c5479d3cf99a
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-01 22:13:20 +0100

    men

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,17 @@
 #!/usr/bin/node
-// my rectangle  Writing to stderr using console.error
 module.exports = class Rectangle{
     constructor(w,h) {
         if (w > 0 && h >0) { [this.width, this.height] = [w, h]} 
     }
 
     print() {
-        for (let i = 0; i <this.height; i++) 
+        for (let i = 0; i <this.height; i +=1) 
         console.log("x".repeat(this.width));
         }
 };
 
-// Writing to stderr using console.error
-console.error('This is an error message.');
+// Writing stderr using console.error
+console.error();
 
 
 
