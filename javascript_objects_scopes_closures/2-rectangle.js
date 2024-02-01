eb51218d5a7081d9efc4b44e2801aaeca05551b0
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-01 19:45:52 +0100

    champ

---
+++ javascript_objects_scopes_closures/2-rectangle.js
@@ -0,0 +1,7 @@
+module.exports = class Rectangle{
+    constructor(w,h) {
+       this.width = (typeof w === 'number' && w > 0) ? w : undefined;
+       this.height = (typeof h === 'number' && h > 0) ? h : undefined;
+    }
+}
+
