e7a099d4cd04b239a8a756c4696f999954971b8b
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-01 18:12:02 +0100

    pvv

---
+++ javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,8 @@
+const firstline = "C is fun"
+console.log(firstline)
+
+const secondline = "Python is cool"
+console.log(secondline)
+
+const thirdline = "JavaScript is amazing"
+console.log(thirdline) 
