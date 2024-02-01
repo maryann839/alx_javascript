3cf6e81e40e69a7accaf9097ef0beb47641eb497
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-01 23:16:46 +0100

    width

---
+++ javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+module.eports = class Square extends require('./5-rectangle.js') {
+    charPrint (c) {
+        if (c === undefined) {
+            this.print();
+        } else {
+            for (let i = 0; i < this.height; i += 1) 
+            console.log(c.repeat(this.width));
+        }
+    }
+}
