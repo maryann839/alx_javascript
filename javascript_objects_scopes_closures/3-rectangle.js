5d2200f16116f47925f50652407e4c0e8808ae72
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-01 22:49:20 +0100

    comment

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -1,4 +1,9 @@
 #!/usr/bin/node
+/*You must use the class notation for defining your class
+The constructor must take 2 arguments: w and h
+Initialize the instance attribute width with the value of w
+Initialize the instance attribute height with the value of h
+*/
 module.exports = class Rectangle{
     constructor(w,h) {
         if (w > 0 && h >0) { [this.width, this.height] = [w, h]} 
