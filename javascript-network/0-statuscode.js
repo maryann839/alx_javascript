b16a92ddfb41c2eb67c1a9681070dc16864503e3
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-08 22:59:28 +0100

    status

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,8 +1,22 @@
 #!/usr/bin/node
 
 const request = require('request');
-const url = 'https://swapi.dev/api/'
+const url = 'https://swapi-api.alx-tools.com/api/films/'
 request.get(url, process.argv[2])
     .on('response', function (response) {
     console.log("code:", response.statusCode);
 });
+
+
+
+    
+
+
+
+
+
+
+// req.get(url, {encoding : 'utf-8'})
+// .on('response', response =>{
+//     console.log("code:", response.statusCode);
+// })
