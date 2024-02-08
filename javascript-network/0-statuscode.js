00fcc1e9ae173fe5ad7cb28d2f3258629fc9b29e
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-08 22:56:45 +0100

    stat

--- javascript-network/0-statuscode.js
+++ javascript-network/0-statuscode.js
@@ -1,15 +1,8 @@
 #!/usr/bin/node
 
-const request = require("request")
-const endpoint = 'https://swapi.dev/api/films/1';
-
-request(endpoint, function (error, response) {
-    console.log("code:", response.statusCode)
+const request = require('request');
+const url = 'https://swapi.dev/api/'
+request.get(url, process.argv[2])
+    .on('response', function (response) {
+    console.log("code:", response.statusCode);
 });
-    
-
-
-
-
-
-
