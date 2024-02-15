9d9a9ab6b045386c4a7f4e5f85d81319b0cdd66f
maryann839 <maryannokoye839@gmail.com>
2024-02-15 22:44:26 +0100

    push

---
+++ Cookies_local_storage/src/index.js
@@ -0,0 +1,13 @@
+function getCookie(cookieName) {
+    var name = cookieName + '=';
+    var decodedCookie = decodeURIComponent(document.cookie);
+    var cookieArray = decodedCookie.split(';');
+
+    for (var i = 0; i < cookieArray.length; i++) {
+        var cookie = cookieArray[i].trim();
+        if (cookie.indexOf(name) == 0) {
+            return cookie.sustring(name.lenght, cookie.length);
+        }
+    }
+    return '';
+}
