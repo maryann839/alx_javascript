43b2db1e475fdd19e442cbab17d559726a8ea65c
maryann839 <149114655+maryann839@users.noreply.github.com>
2024-02-23 15:07:57 +0100

    pooo

--- javascript_objects_scopes_closures/3-rectangle.js
+++ javascript_objects_scopes_closures/3-rectangle.js
@@ -6,14 +6,15 @@ Initialize the instance attribute height with the value of h
 */
 module.exports = class Rectangle {
     constructor(w,h) {
-        if (w > 0 && h >0) {
-             [this.width, this.height] = [w, h];} 
+        if (w <= 0 || h <= 0) {
+              return{}
+        } 
     };
 
     print() {
         for (let i = 0; i <this.height; i +=1) 
         console.log("X".repeat(this.width));
-        };
+        }; 
 };
 
 
