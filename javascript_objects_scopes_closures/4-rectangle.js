ccfcba5a8564c4ffbcbffd6023573eb84b625489
maryann839 <maryannokoye839@gmail.com>
2024-02-23 15:01:20 +0100

    java

--- javascript_objects_scopes_closures/4-rectangle.js
+++ javascript_objects_scopes_closures/4-rectangle.js
@@ -2,13 +2,19 @@
 
 module.exports = class Rectangle{
     constructor(w,h) {
-        if (w > 0 && h >0) { [this.width, this.height] = [w, h]} 
-    }
-
+        this.width = w
+        this.height= h
+        
+        if (w <= 0 || h <= 0) { 
+            return{}
+        } 
+    };
+     
+   
     print() {
-        for (let i = 0; i <this.height; i += 1) 
-        console.log("x".repeat(this.width));
-     }
+        for (let i = 0; i <this.height; i += 1);
+        console.log("X".repeat(this.width));
+     };
     
     rotate() {
             [this.width, this.height] =  [this.width, this.height]
